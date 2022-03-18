700db4b2e3a83afadd06d9c43dcc8700358392c8
Guilherme TJ <guips1998@gmail.com>
2022-03-18 12:16:51 -0300

    Criando entidade Room

---
+++ backend/Model/Entity/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace Hotel_Management.Model.Entity
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomType { get; set; }
+        public int Price { get; set; }
+        public int Hotel_id { get; set; }
+        public Hotel Hotel { get; set; }
+    }
+}
