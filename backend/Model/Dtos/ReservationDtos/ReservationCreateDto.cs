00b4ccad1cb0446588781064b67f4c53555e11d9
Guilherme TJ <guips1998@gmail.com>
2022-03-21 18:09:54 -0300

    Criando dto de Reservation

---
+++ backend/Model/Dtos/ReservationDtos/ReservationCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Hotel_Management.Model.Dtos.ReservationDtos
+{
+    public class ReservationCreateDto
+    {
+        public string Status { get; set; }
+        public int Client_id { get; set; }
+        public int Room_id { get; set; }
+    }
+}
---
+++ backend/Model/Dtos/ReservationDtos/ReservationDto.cs
@@ -0,0 +1,13 @@
+﻿using Hotel_Management.Model.Dtos.ClientDtos;
+using Hotel_Management.Model.Dtos.RoomDtos;
+
+namespace Hotel_Management.Model.Dtos.ReservationDtos
+{
+    public class ReservationDto
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public ClientDto Client { get; set; }
+        public RoomDto Room { get; set; }
+    }
+}
