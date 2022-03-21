21ac4a6c0b6625afb63175dd94aa3866b692488d
Guilherme TJ <guips1998@gmail.com>
2022-03-21 16:13:31 -0300

    Criando dto de clients

--- backend/Model/Dtos/ClientDtos/ClientDto.cs
+++ backend/Model/Dtos/ClientDtos/ClientDto.cs
@@ -2,5 +2,11 @@
 {
     public class ClientDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+        public DateTime? DeletionAt { get; set; }
     }
 }
--- backend/Model/Dtos/ClientDtos/ClientUpdateDto.cs
+++ backend/Model/Dtos/ClientDtos/ClientUpdateDto.cs
@@ -1,12 +1,10 @@
 ﻿namespace Hotel_Management.Model.Dtos.ClientDtos
 {
-    public class ClientDetailsDto
+    public class ClientUpdateDto
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Phone { get; set; }
         public string Adress { get; set; }
-        public DateTime? DeletionAt { get; set; }
     }
 }
