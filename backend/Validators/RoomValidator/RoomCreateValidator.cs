4d14c82e077a6b654e5e1f4a122852c7cc2a7b94
Guilherme TJ <guips1998@gmail.com>
2022-03-31 19:38:24 -0300

    Validação de room

---
+++ backend/Validators/RoomValidator/RoomCreateValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Hotel_Management.Model.Dtos.RoomDtos;
+
+namespace Hotel_Management.Validators.RoomValidator
+{
+    public class RoomCreateValidator : AbstractValidator<RoomCreateDto>
+    {
+        public RoomCreateValidator()
+        {
+            RuleFor(x => x.RoomType).NotNull().NotEmpty().Must(RoomTypes).WithMessage("Tipo de quarto precisa ser Standard, Master ou Deluxe");
+            RuleFor(x => x.Price).NotNull().NotEmpty().ScalePrecision(2,10,true);
+        }
+
+        private bool RoomTypes(string types)
+        {
+            return types == "Standard" || types == "Master" || types == "Deluxe";
+        }
+    }
+}
