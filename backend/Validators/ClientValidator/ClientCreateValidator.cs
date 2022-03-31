9cba3f4276b99b3646bad0a7d4e6b4a0b6afb01d
Guilherme TJ <guips1998@gmail.com>
2022-03-31 20:07:00 -0300

    Client Validator

---
+++ backend/Validators/ClientValidator/ClientCreateValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Hotel_Management.Model.Dtos.ClientDtos;
+
+namespace Hotel_Management.Validators.ClientValidator
+{
+    public class ClientCreateValidator : AbstractValidator<ClientCreateDto>
+    {
+        ClientCreateValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().NotNull().Length(2, 10);
+            RuleFor(x => x.LastName).NotEmpty().NotNull().Length(2, 10);
+            RuleFor(x => x.Phone).NotEmpty().NotNull().Matches("[2-9][0-9]{10}");
+            RuleFor(x => x.Address).NotEmpty().NotNull();
+        }
+    }
+}
---
+++ backend/Validators/ClientValidator/ClientUpdateValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Hotel_Management.Model.Dtos.ClientDtos;
+
+namespace Hotel_Management.Validators.ClientValidator
+{
+    public class ClientUpdateValidator : AbstractValidator<ClientUpdateDto>
+    {
+        public ClientUpdateValidator()
+        {
+            RuleFor(x => x.Name).Length(2, 10);
+            RuleFor(x => x.LastName).Length(2, 10);
+            RuleFor(x => x.Phone).Matches("[2-9][0-9]{10}");
+            RuleFor(x => x.Address).Length(2, 40);
+        }
+ 
+    }
+}
