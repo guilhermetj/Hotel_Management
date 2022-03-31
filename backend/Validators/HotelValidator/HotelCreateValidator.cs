58c4e6da75165092f0ff914dd3026b25a71782c5
Guilherme TJ <guips1998@gmail.com>
2022-03-31 15:28:56 -0300

    Validação de hoteis

---
+++ backend/Validators/HotelValidator/HotelCreateValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Hotel_Management.Model.Dtos.HotelDtos;
+
+namespace Hotel_Management.Validators.HotelValidator
+{
+    public class HotelCreateValidator : AbstractValidator<HotelCreateDto>
+    {
+        public HotelCreateValidator()
+        {
+            RuleFor(x => x.Name).NotNull().NotEmpty().Length(2, 30);
+            RuleFor(x => x.Location).NotNull().NotEmpty().Length(2, 10);
+            RuleFor(x => x.NumRooms).NotNull().NotEmpty().InclusiveBetween(1, 100);
+        }
+    }
+}
---
+++ backend/Validators/HotelValidator/HotelUpdateValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Hotel_Management.Model.Dtos.HotelDtos;
+
+namespace Hotel_Management.Validators.HotelValidator
+{
+    public class HotelUpdateValidator : AbstractValidator<HotelUpdateDto>
+    {
+        public HotelUpdateValidator()
+        {
+            RuleFor(x => x.Name).Length(2, 30);
+            RuleFor(x => x.Location).Length(2, 10);
+            RuleFor(x => x.NumRooms).NotNull().NotEmpty().InclusiveBetween(1, 100);
+        }
+    }
+}
